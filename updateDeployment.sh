9f171ccd56b874e12c35fd0d62d43279e47fd513
Shubhranshu Panda <37568077+shubhranshupanda@users.noreply.github.com>
2025-06-28 18:39:54 +0530

    Update updateDeployment.sh

--- updateDeployment.sh
+++ updateDeployment.sh
@@ -6,6 +6,9 @@ eval $(minikube docker-env)
 # Build Docker image
 docker build --no-cache -t freetricksworld/helloworld:latest .
 
+#update to docker hub
+docker push freetricksworld/helloworld:latest
+
 # Restart the Kubernetes deployment
 kubectl rollout restart deployment mytestapp
 
